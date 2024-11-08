34485e916e8bdcb0f4b3dcedd18605c649007a26
Neel Sharma <nsdj.sharma@gmail.com>
2024-11-08 20:50:55 +1100

    add comments to explain the working mecanum drive opmode

--- MecanumWheels_Working.java
+++ MecanumWheels_Working.java
@@ -30,6 +30,9 @@ public class MecanumWheels_Working extends LinearOpMode {
         frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
 
+        // btw, yall had reversed a motor that shouldn't have been reversed.
+        // for future reference, if the robot isn't moving correctly, it's usually a good idea to prop the robot up so that the wheels don't touch the floor so you can inspect if wheels are turning the right way or not. that's how i discovered this.
+        // - neel
         frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         imu = hardwareMap.get(IMU.class, "imu");
@@ -49,16 +52,23 @@ public class MecanumWheels_Working extends LinearOpMode {
                 imu.resetYaw();
             }
 
+            // get joystick inputs
             double inputY = -gamepad1.left_stick_y;
             double inputX = gamepad1.left_stick_x;
             double rot = gamepad1.right_stick_x;
 
-            // convert robot relative velocity to field relative velocity via 2d vector rotation
-            // https://en.wikipedia.org/wiki/Rotation_matrix
+            // this converts the input velocity, which is robot-relative, to field relative velocity via 2d vector rotation (https://en.wikipedia.org/wiki/Rotation_matrix)
+            // this allows the driver to control the robot using motions relative to the field rather than the robot.
+            // i assume this is what you were trying to do in the other files, but... failed.
+            // also, ftclib has lots of classes that do these calculations for you. if you want to use those, find docs for the MecanumDriveKinematics and ChassisSpeeds classes, among others.
+            // alternatively, feel free to ping me on the discord if you get stuck.
+            // - neel
             double yaw = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
             double y = inputX * Math.cos(yaw) + inputY * Math.sin(yaw);
             double x = -inputX * Math.sin(yaw) + inputY * Math.cos(yaw);
 
+            // fyi the rest of this code is mainly taken from the example mecanum drive code that ftc provides.
+            // - neel
             double frontLeftPower = y + x + rot;
             double frontRightPower = y - x - rot;
             double backLeftPower = y - x + rot;
@@ -81,7 +91,6 @@ public class MecanumWheels_Working extends LinearOpMode {
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
 
-            // telemetry
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Yaw Angle (deg)", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
             telemetry.addData("FL Power", frontLeftPower);
