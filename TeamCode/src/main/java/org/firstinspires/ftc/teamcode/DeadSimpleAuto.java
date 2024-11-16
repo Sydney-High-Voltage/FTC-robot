c6e6e88d6f7f1ced17f2ab2c13c7c88ee29f0a78
Neel Sharma <nsdj.sharma@gmail.com>
2024-11-17 09:21:09 +1100

    add dead simple auto (just move right)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DeadSimpleAuto.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Dead Simple Auto. Just moves right to get the parking points.
+ * Place the robot against the wall, to the left of the observation zone, facing into the center of the field.
+ */
+@Autonomous(name = "Dead Simple Auto")
+public class DeadSimpleAuto extends LinearOpMode {
+    private final ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+            if (runtime.seconds() < 2) {
+                drive(0.5, 0, 0);
+            } else {
+                drive(0, 0, 0);
+            }
+            telemetry.update();
+        }
+    }
+
+    public void drive(double x, double y, double r) {
+        double frontLeftPower = x - y - r;
+        double frontRightPower = x + y + r;
+        double backLeftPower = x + y - r;
+        double backRightPower = x - y + r;
+
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
+        max = Math.max(max, Math.abs(backLeftPower));
+        max = Math.max(max, Math.abs(backRightPower));
+
+        if (max > 1.0) {
+            frontLeftPower /= max;
+            frontRightPower /= max;
+            backLeftPower /= max;
+            backRightPower /= max;
+        }
+
+        // Send powers to the wheels.
+        frontLeftMotor.setPower(frontLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backLeftMotor.setPower(backLeftPower);
+        backRightMotor.setPower(backRightPower);
+
+        telemetry.addData("FL Power", frontLeftPower);
+        telemetry.addData("FR Power", frontRightPower);
+        telemetry.addData("BL Power", backLeftPower);
+        telemetry.addData("BR Power", backRightPower);
+    }
+}
